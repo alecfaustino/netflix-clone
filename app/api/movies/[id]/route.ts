81aee8f5f791fa1c42616eb55c0a80d8ed6f0d25
Alec Faustino <alecfaustino@outlook.com>
2025-08-26 12:00:34 -0600

    api type error nextRequest

--- app/api/movies/[id]/route.ts
+++ app/api/movies/[id]/route.ts
@@ -1,7 +1,7 @@
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 
-export async function GET(context: { params: Promise<{ id: string }> }) {
-  const { id } = await context.params;
+export async function GET(request: NextRequest, context: { params: { id: string } }) {
+  const { id } = context.params;
 
   const res = await fetch(
     `https://api.themoviedb.org/3/movie/${id}`,
--- app/api/movies/[id]/similar/route.ts
+++ app/api/movies/[id]/similar/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
-export async function GET(context: { params: Promise<{id: string}> }) {
-  const { id } = await context.params;
+export async function GET(context: { params: { id: string } }) {
+  const { id } = context.params;
 
   const res = await fetch(`https://api.themoviedb.org/3/movie/${id}/similar`, {
     headers: {
