8b2e77dba161c535d9701cf04f7001f2d2aad461
Alec Faustino <alecfaustino@outlook.com>
2025-09-28 11:59:15 -0600

    fix /movies to have shadcn card instead of materialui

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,3 +1,5 @@
+import { Card, CardFooter } from "@/components/ui/card";
+import Image from "next/image";
 import Link from "next/link";
 
 export default async function Movies() {
@@ -16,34 +18,36 @@ https://api.themoviedb.org/3/discover/movie`,
   const movies = data.results;
 
   return (
-    // <main>
-    //   <h4 className="text-center text-4xl font-semibold p-4">
-    //     Discover Movies
-    //   </h4>
-    //   <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 p-4 mt-0 pt-0">
-    //     {movies.map(
-    //       (movie: { id: number; title: string; poster_path: string }) => (
-    //         <div className="col-span-1" key={movie.id}>
-    //           <Link href={`/movies/${movie.id}`}>
-    //             <Card className="hover:scale-105 transition-transform">
-    //               {movie.poster_path && (
-    //                 <CardMedia
-    //                   component="img"
-    //                   image={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-    //                   alt={movie.title}
-    //                 />
-    //               )}
-    //               <CardContent className="text-center h-24">
-    //                 <h1 className="text-lg font-semibold">{movie.title}</h1>
-    //               </CardContent>
-    //             </Card>
-    //           </Link>
-    //         </div>
-    //       )
-    //     )}
-    //   </div>
-    // </main>
-
-    <></>
+    <div className="mb-16">
+      <h4 className="text-center text-4xl font-semibold p-4">
+        Discover Movies
+      </h4>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 p-4 mt-0 pt-0">
+        {movies.map(
+          (movie: { id: number; title: string; poster_path: string }) => (
+            <div className="col-span-1" key={movie.id}>
+              <Link href={`/movies/${movie.id}`}>
+                <Card className="hover:scale-105 transition-transform ">
+                  {movie.poster_path && (
+                    <Image
+                      src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                      alt={movie.title}
+                      width={500}
+                      height={750}
+                      className="w-full h-auto rounded p-2"
+                    />
+                  )}
+                  <CardFooter className="text-center h-16 justify-center">
+                    <h1 className="text-lg font-semibold text-center">
+                      {movie.title}
+                    </h1>
+                  </CardFooter>
+                </Card>
+              </Link>
+            </div>
+          )
+        )}
+      </div>
+    </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1>Welcome to FavFlix</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen gap-6">
+      <h1 className="text-5xl font-bold">Welcome to FavFlix</h1>
       <Button>
         <Link href="/movies">Get Started</Link>
       </Button>
