4aa15ddf91ac55e9e7add52e9ed9203543fad982
Alec Faustino <alecfaustino@outlook.com>
2025-09-28 09:44:53 -0600

    remove material UI from searchbar component

--- components/ui/searchbar.tsx
+++ components/ui/searchbar.tsx
@@ -17,21 +17,16 @@ export default function SearchBar() {
   };
 
   return (
-    <div>
+    <div className="flex justify-center w-full">
       <form
         onSubmit={onSubmit}
-        className="flex items-center rounded-full px-3 py-1 shadow transition w-full max-w-xs sm:max-w-md border-2 border-gray-700">
+        className="flex items-center rounded-full px-3 py-1 shadow transition max-w-xs sm:max-w-md border-2 border-gray-700">
         <Input
           onChange={searchMovie}
           value={searchTerm}
-          placeholder="Search movies..."
-          className="bg-transparent border-0 border-none ring-0 text-white placeholder-gray-400 focus:ring-0 focus:outline-none focus:border-0 focus:border-none focus:ring-transparent flex-1 min-w-0"
+          placeholder="Search"
+          className="flex-1 bg-transparent border-0 text-white placeholder-gray-400 focus:outline-none focus:ring-0 focus-visible:ring-0 focus:border-gray-500"
         />
-        <button
-          type="submit"
-          className="ml-2 px-4 py-2 rounded-full font-semibold transition">
-          Search
-        </button>
       </form>
     </div>
   );
