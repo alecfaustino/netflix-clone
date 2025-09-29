3a6b427ebac150d6bfa02e04dbadf938a184b994
Alec Faustino <alecfaustino@outlook.com>
2025-09-29 15:28:04 -0600

    fix deployment errors - left behind theme registry import

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
-import ThemeRegistry from "./ThemeRegistry";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
