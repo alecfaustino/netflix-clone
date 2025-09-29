27f2c980b316e17446ac70e572482b9bd2c7525b
Alec Faustino <alecfaustino@outlook.com>
2025-09-29 15:29:28 -0600

    center the text on the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen gap-6">
-      <h1 className="text-5xl font-bold">Welcome to FavFlix</h1>
+      <h1 className="text-5xl font-bold text-center">Welcome to FavFlix</h1>
       <Button>
         <Link href="/movies">Get Started</Link>
       </Button>
